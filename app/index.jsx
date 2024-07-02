3a1d4b04f5d8f23a8ca2a4e2cee4e7a27afa70ae
Feras Nasr <feras94@protonmail.com>
2024-07-02 20:07:39 +0200

    configure nativewind and style the intital app interface (add logo and sign in/ sign up buttons

---
+++ app/index.jsx
@@ -0,0 +1,31 @@
+import { Link } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import {
+  Image,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-customColor">
+      <Image source={require('../assets/Diabuddy-logo.png')} />
+      {/* <Text className="text-3xl color-white">Welcome to Diabuddy</Text> */}
+      <StatusBar className="color-black" />
+      <View className="flex-row space-x-4">
+        <TouchableOpacity className="bg-[#05664F] px-4 py-2 rounded-md">
+          <Link href={'/profile'} className="text-white text-xl">
+            Sign In
+          </Link>
+        </TouchableOpacity>
+        <TouchableOpacity className="bg-[#14EDB9] px-4 py-2 rounded-md">
+          <Link href={'/profile'} className="text-white text-xl">
+            Join Now
+          </Link>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
